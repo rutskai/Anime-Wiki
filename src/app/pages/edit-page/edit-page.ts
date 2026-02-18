7b8f739dce51df94808c89cc0569adf7c09c8eb1
Ruth <rcolladogarcia@iessonferrer.net>
2026-02-18 23:16:18 +0100

    Add edit page

---
+++ src/app/pages/edit-page/edit-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormAnime } from "../../components/form-anime/form-anime";
+
+@Component({
+  selector: 'app-edit-page',
+  imports: [FormAnime],
+  templateUrl: './edit-page.html',
+  styleUrl: './edit-page.css',
+})
+export class EditPage {
+
+}
