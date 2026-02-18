250936f21589450cff791b1769b939b5ceecc652
Ruth <rcolladogarcia@iessonferrer.net>
2026-02-18 23:16:41 +0100

    Update errors form

--- src/app/components/form-anime/form-anime.ts
+++ src/app/components/form-anime/form-anime.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Anime } from '../../models/anime.model';
 import { AnimeService } from '../../services/anime-service';
 
@@ -13,10 +13,11 @@ import { AnimeService } from '../../services/anime-service';
 export class FormAnime implements OnInit{
 
   animeForm!: FormGroup;
+  id:string | null = null;
 
   statusOptions = ['En emisión', 'Finalizado', 'Pendiente'];
 
-  constructor(private animeService: AnimeService,private router: Router) {}
+  constructor(private animeService: AnimeService,private router: Router, private route: ActivatedRoute) {}
    
  ngOnInit(): void {
     this.animeForm = new FormGroup({
@@ -24,15 +25,32 @@ export class FormAnime implements OnInit{
       synopsis: new FormControl('', [Validators.required, Validators.minLength(10)]),
       genre: new FormControl('', [Validators.required]),
       episodes: new FormControl(1, [Validators.required, Validators.min(1)]),
-      score: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(10)]),
+      score: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(100)]),
       status: new FormControl('Pendiente', [Validators.required]),
       imageUrl: new FormControl('', [Validators.required, Validators.pattern('https?://.+')]),
       year: new FormControl(2024, [Validators.required, Validators.min(1960), Validators.max(2030)])
     });
 
+    this.id = this.route.snapshot.paramMap.get('id');
+    if(this.id){
+       this.animeService.getById(this.id).subscribe({
+        next: (data) => {
+          this.animeForm.patchValue(data);
+        },
+        error: (err) => console.error('Error al cargar anime:', err)
+      });
+    }
   }
 
   onSubmit(): void {
+    if(this.id){
+      this.update();
+    }else{
+      this.create();
+    }
+  }
+
+  create(){
     if (this.animeForm.valid) {
       const animeData: Anime = this.animeForm.value;
       
@@ -45,6 +63,16 @@ export class FormAnime implements OnInit{
         }
       });
     }
+  }
+
+ update(): void {
+  if(this.animeForm.valid){
+    const animeData: Anime = this.animeForm.value;
+    this.animeService.update(this.id!, animeData).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (err) => console.error('Error al actualizar:', err)
+    });
+  }
   }
 
    cancel(): void {
